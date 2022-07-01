fe5af1c700b0a1759663f395a66328789540cdc4
Favio <83928038+FavioReinaldo@users.noreply.github.com>
2022-07-01 15:55:56 -0300

    add or edit more js functions

--- script.js
+++ script.js
@@ -1,38 +1,67 @@
+//Game logic
+let playerScore = 0;
+let computerScore = 0;
+
+let playerChoiceBtn = document.querySelectorAll(".playerChoiceBtn"); //this generates an array with the 3 buttons
+playerChoiceBtn.forEach((button) => {
+  button.addEventListener("click", game(button.value));
+});
 
 
-function computerPlay(){
-    let max = 3;
-    return Math.floor(Math.random() * max);
-    //0 ‘Rock’, 1 ‘Paper’ or 2 ‘Scissors’
+function game(playerSelection) {
+  playRound(playerSelection, computerPlay());
+  checkWinner();
 }
 
-function playRound(playerSelection, computerSelection){
-    let result="";
-    computerSelection = computerPlay();
-    
-    if(playerSelection === computerSelection){
-        result = "Its a Draw!"
-    }else if(playerSelection === 0){
-        if(computerSelection === 1){
-            result = "You Lose! Paper beats Rock";
-        }else{
-            result = "You Win! Rock beats Scissors";
-        }
-    }else if(playerSelection === 1){
-        if(computerSelection === 0){
-            result = "You Win! Paper beats Rock";
-        }else{
-            result = "You Lose! Scissors beats Paper";
-        }
-    }else{
-        if(computerSelection === 0){
-            result = "You Lose! Rock beats Scissors";
-        }else{
-            result = "You Win! Scissors beats Paper";
-        }
+//Functions
+
+function computerPlay() {
+  let choices = ["Rock", "Paper", "Scissors"];
+  return choices[Math.floor(Math.random() * choices.length)];
+}
+
+function playRound(playerSelection, computerSelection) {
+  let result = "";
+  let roundWinCombo = `${playerSelection}-${computerSelection}`;
+  let playerWinCombo = ["Rock-Scissors", "Paper-Rock", "Scissors-Paper"];
+
+  if (playerSelection === computerSelection) {
+    playerScore++;
+    computerScore++;
+    result = "Its a Tie!";
+    updateDOM(playerScore, computerScore, result);
+  } else if (playerWinCombo.includes(roundWinCombo)) {
+    playerScore++;
+    result = `You win! ${playerSelection} beats ${computerSelection}`;
+    updateDOM(playerScore, computerScore, result);
+  } else {
+    result = `You lose! ${computerSelection} beats ${playerSelection}`;
+    computerScore++;
+    updateDOM(playerScore, computerScore, result);
+  }
+
+  return result;
+}
+
+function updateDOM(playerScore, computerScore, result) {
+  document.querySelector("#playerScore").innerHTML = `${playerScore}`;
+  document.querySelector("#computerScore").innerHTML = `${computerScore}`;
+  document.querySelector("#result").innerHTML = `${result}`;
+}
+
+function checkWinner() {
+  if (computerScore === 5 || playerScore === 5) {
+    if (computerScore === playerScore) {
+      document.querySelector("#result").innerHTML = "The game is a Tie!";
+    } else if (playerScore > computerScore) {
+      document.querySelector("#result").innerHTML = "You win the game!";
+    } else {
+      document.querySelector("#result").innerHTML = "You lost the game!";
     }
-    return result;
+  }
+  endGame();
 }
 
-
-
+function endGame(){
+    
+}
