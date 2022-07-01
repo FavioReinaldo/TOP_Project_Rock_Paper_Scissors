80eedc879b76f5455ced53347f6a04a108be4154
Favio <83928038+FavioReinaldo@users.noreply.github.com>
2022-07-01 12:34:23 -0300

    add computerPlay() and playRoud()

--- script.js
+++ script.js
@@ -1,10 +1,38 @@
 
 
 function computerPlay(){
-
+    let max = 3;
+    return Math.floor(Math.random() * max);
+    //0 ‘Rock’, 1 ‘Paper’ or 2 ‘Scissors’
 }
 
 function playRound(playerSelection, computerSelection){
-
-    return "You Lose! Paper beats Rock";
+    let result="";
+    computerSelection = computerPlay();
+    
+    if(playerSelection === computerSelection){
+        result = "Its a Draw!"
+    }else if(playerSelection === 0){
+        if(computerSelection === 1){
+            result = "You Lose! Paper beats Rock";
+        }else{
+            result = "You Win! Rock beats Scissors";
+        }
+    }else if(playerSelection === 1){
+        if(computerSelection === 0){
+            result = "You Win! Paper beats Rock";
+        }else{
+            result = "You Lose! Scissors beats Paper";
+        }
+    }else{
+        if(computerSelection === 0){
+            result = "You Lose! Rock beats Scissors";
+        }else{
+            result = "You Win! Scissors beats Paper";
+        }
+    }
+    return result;
 }
+
+
+
